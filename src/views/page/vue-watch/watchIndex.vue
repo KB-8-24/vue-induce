861b3ed84bf4f0aee181bd4c41f322f01684c6d0
yanjingsong <1559699429@qq.com>
2022-03-25 16:01:23 +0800

    update: 跟新

--- src/views/page/vue-watch/watchIndex.vue
+++ src/views/page/vue-watch/watchIndex.vue
@@ -6,15 +6,23 @@
 export default {
     // watch 可以监听data 和 props 里面的数据
     watch: {
+        // 简写方式
         state1(newState, oldState) {
             console.log(newState, oldState, "state1")
         },
+        // 完整写法
         state2: {
             handler(newState, oldState) {
                 console.log(newState, oldState, "state2")
             },
             deep: true, // 普通的watch方法无法监听到对象内部属性的改变,deep属性对对象进行深度监听
             immediate: true // 最初绑定值的时候也执行函数，则就需要用到immediate属性
+        },
+        // 监听对象中的某个属性
+        "state1.a": {
+            handler(newState, oldState) {
+                console.log(newState, oldState, "state1")
+            }
         }
     },
     data() {
